5b3de3e1be3d5ffa0fe43a7c48309c7e4957c416
Yngrid Santos Moreira de Oliveira <yngrid-hp>
2015-03-24 17:24:42 -0300

    Exercícios

---
+++ Exercícios/semarray/semarray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem vindo ao calculador de médias :D , insira 0 a qualquer momento para sair");
+            int numero = 0;
+            int media = 0;
+
+            for (int i = 1; i != 0; i++)
+            {
+                Console.WriteLine("Insira o número desejado");
+                numero = int.Parse(Console.ReadLine());
+
+                media = (media + numero) / i;
+                if (numero == 0)
+                {
+                    Console.Write("A média dos números é ;", media);
+                    Console.ReadKey();
+                   
+                }
+
+            }  
+        }
+    }
+}
