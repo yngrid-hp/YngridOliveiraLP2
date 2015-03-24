4cb82b21ccd2591dbc828efc29a3aef391614793
Yngrid Santos Moreira de Oliveira <yngrid-hp>
2015-03-24 17:23:54 -0300

    Exercí

---
+++ Exercícios/comarray/comarray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace comarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList array = new ArrayList();
+            int numero = 1;
+            int media = 0;
+            int medio = 0;
+
+
+
+            for (int i = numero; i != 0; i++)
+            {
+
+                Console.WriteLine("Insira o número");
+                numero = int.Parse(Console.ReadLine());
+                array.Add(numero);                
+                media += numero;    
+                medio ++;
+                medio = media / medio;
+                if (numero == 0)
+                    
+                {
+                    Console.WriteLine("A Média é: {0}", medio);
+                    Console.ReadKey();
+                }
+            }
+            
+        }
+    }
+}
