47b0006a55871dc97b98c3b60638e15eead96e63
Anderson <And174_br@yahoo.com.br>
2015-07-07 18:00:02 -0300

    d

---
+++ Exercícios/Exercicio Carro Classe/Exercicio Carro Classe/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro
+{
+    public class Carro
+    {
+        public string placa;
+        public string modelo;
+        public double consumoKmL;
+        public double qtdCombustivelL;
+
+        public double AutonomiaKm() // Método. Esse não tem parâmetro porque não precisa de nenhuma outra informação
+        {
+            double autonomia = consumoKmL * qtdCombustivelL;
+            return autonomia;
+
+        }
+
+
+
+
+    }
+}
