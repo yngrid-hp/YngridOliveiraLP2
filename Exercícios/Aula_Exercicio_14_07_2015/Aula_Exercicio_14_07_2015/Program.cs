6ee2df2196b9d6e4d1709f0793e9cc1a55b1723f
Anderson Luiz De Melo Viana <And174_br@yahoo.com.br>
2015-07-14 17:26:17 -0300

    Get e Set

---
+++ Exercícios/Aula_Exercicio_14_07_2015/Aula_Exercicio_14_07_2015/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Exercicio_14_07_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fido = new Cachorro();
+        fido._tamanhoCm = 95;
+        fido.SetNome("Fido");
+        fido.SetPeso(42.3);
+            Console.WriteLine("Nome: {0}", fido.GetNome());
+            Console.WriteLine("Peso: {0}", fido.GetPeso());
+            Console.WriteLine("Tamanho (m): {0},",fido.TamanhoMetro(fido._tamanhoCm));
+        Console.WriteLine("Quantidade diária de ração : {0}", fido.RacaoDiariaKg());
+        }
+    }
+}
---
+++ Exercícios/Aula_Exercicio_14_07_2015/Aula_Exercicio_14_07_2015/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aula_Exercicio_14_07_2015")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Aula_Exercicio_14_07_2015")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b62f7ae-b804-49e1-ba9a-c646b57ece19")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
